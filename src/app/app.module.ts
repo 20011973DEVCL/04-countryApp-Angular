bca3c5473894c64ff5be8a107a1473636f095b7a
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-08-17 17:08:37 -0400

    new pages country , capital and cities order by module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
   ],
   imports: [
     AppRoutingModule,
--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { CountriesRoutinModule } from './countries-routing.modules';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRoutinModule,
+    SharedModule
   ]
 })
 export class CountriesModule { }
